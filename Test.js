e0f13d46a3512440461da5a42500e6f792746394
CoderMANE321 <108749886+CoderMANE321@users.noreply.github.com>
2023-12-09 22:11:42 -0800

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/Bruh')
+      .then(res => {
+        console.log(res.data); // Check the response data
+        setMessage(JSON.stringify(res.data, null, 2)); // Convert to a string for display
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  
+  
+  return (
+    <div className="App">
+      <h2>Test</h2>
+      <pre>
+        <code>
+          {message}
+        </code>
+      </pre>
+    </div>
+  );
+}
+
+
+export default App;
